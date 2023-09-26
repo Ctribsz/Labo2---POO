491cc8a1a4fbbfca13f609e57bc8d09277d62177
Ctribsz <122636369+Ctribsz@users.noreply.github.com>
2023-09-26 05:31:31 -0600

    programa terminado

--- Drive.java
+++ Drive.java
@@ -17,8 +17,8 @@ public class Drive {
             HelpMethods hp = new HelpMethods();
             System.out.println("----MENU-----");
             System.out.println("1. Ingresar notas");
-            System.out.println("2. Ver promedio general");
-            System.out.println("3. Ver promedio de las clases");
+            System.out.println("2. Ver promedio de las clases");
+            System.out.println("3. Ver promedio general");
             System.out.println("4. Salir.");
 
             Scanner sc = new Scanner(System.in);
@@ -45,6 +45,7 @@ public class Drive {
                         arr2.add(cc);
                     }
                     System.out.println(arr2);
+                    System.out.println("-------------------------------------------");
                     System.out.println("¿Cuantás notas desea ingresar?");
                     int ntc2 = sc.nextInt();
                     System.out.println("Ingrese las notas de la tercera clase: ");
@@ -53,6 +54,7 @@ public class Drive {
                         arr3.add(cc);
                     }
                     System.out.println(arr3);
+                    System.out.println("-------------------------------------------");
                     System.out.println("¿Cuantás notas desea ingresar?");
                     int ntc3 = sc.nextInt();
                     System.out.println("Ingrese las notas de la cuarta clase: ");
@@ -61,9 +63,10 @@ public class Drive {
                         arr4.add(cc);
                     }
                     System.out.println(arr4);
+                    System.out.println("-------------------------------------------");
                     System.out.println("¿Cuantás notas desea ingresar?");
                     int ntc4 = sc.nextInt();
-                    System.out.println("Ingrese las notas de la quinta clase: ");
+                    System.out.println("Ingrese las notas de la quinta clase en (porcentajes): ");
                     for (int i = 0; i < ntc4; i++) {
                         Float cc = sc.nextFloat();
                         arr5.add(cc);
@@ -73,18 +76,68 @@ public class Drive {
                 
                 case 2:
                         System.out.println("Promedio primera clase: ");
-                        System.out.println(hp.calcularMediaArray(arr1));
+                        System.out.println(hp.calcularMediaArray(arr1) + "%");
+                        System.out.println("-------------------------------------------");
                         System.out.println("Promedio segunda clase: ");
-                        System.out.println(hp.calcularMediaArray(arr2));
+                        System.out.println(hp.calcularMediaArray(arr2) + "%");
+                        System.out.println("-------------------------------------------");
                         System.out.println("Promedio tercera clase: ");
-                        System.out.println(hp.calcularMediaArray(arr3));
+                        System.out.println(hp.calcularMediaArray(arr3) + "%");
+                        System.out.println("-------------------------------------------");
                         System.out.println("Promedio cuarta clase: ");
-                        System.out.println(hp.calcularMediaArray(arr4));
+                        System.out.println(hp.calcularMediaArray(arr4) + "%");
+                        System.out.println("-------------------------------------------");
                         System.out.println("Promedio quinta clase: ");
-                        System.out.println(hp.calcularMediaArray(arr5));
+                        System.out.println(hp.calcularMediaArray(arr5) + "%");
+                        System.out.println("-------------------------------------------");
                     break;
                 
                 case 3:
+                    double pr1 = hp.calcularMediaArray(arr1);
+                    double pr2 = hp.calcularMediaArray(arr2);
+                    double pr3 = hp.calcularMediaArray(arr3);
+                    double pr4 = hp.calcularMediaArray(arr4);
+                    double pr5 = hp.calcularMediaArray(arr5);
+
+                    double total = pr1 + pr2 + pr3 + pr4 + pr5;
+                    double prom = total / 5; 
+
+                    System.out.println("El promedio general es de: " + prom);
+                    
+                    System.out.println("-------------------------------------------");
+                    if(pr1 >= 90){
+                        System.out.println("Felicidades tienes un buen promedio en la primer clase ");
+                    } else if (pr1 <= 10){
+                        System.out.println("Cuidado tu promedio esta por debajo de la media en la primer clase");
+                    }
+
+                    System.out.println("-------------------------------------------");
+                    if(pr2 >= 90){
+                        System.out.println("Felicidades tienes un buen promedio en la segunda clase");
+                    } else if (pr2 <= 10){
+                        System.out.println("Cuidado tu promedio esta por debajo de la media en la segunda clase");
+                    }
+
+                    System.out.println("-------------------------------------------");
+                    if(pr3 >= 90){
+                        System.out.println("Felicidades tienes un buen promedio en la tercera clase");
+                    } else if (pr3 <= 10){
+                        System.out.println("Cuidado tu promedio esta por debajo de la media en la tercera clase");
+                    }
+
+                    System.out.println("-------------------------------------------");
+                    if(pr4 >= 90){
+                        System.out.println("Felicidades tienes un buen promedio en la cuarta clase");
+                    } else if (pr4 <= 10){
+                        System.out.println("Cuidado tu promedio esta por debajo de la media en la cuarta clase");
+                    }
+
+                    System.out.println("-------------------------------------------");
+                    if(pr5 >= 90){
+                        System.out.println("Felicidades tienes un buen promedio en la quinta clase");
+                    } else if (pr5 <= 10){
+                        System.out.println("Cuidado tu promedio esta por debajo de la media en la quinta clase");
+                    }
                     break;
 
                 case 4:
